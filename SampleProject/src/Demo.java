cc5f374e74a3409b8b8d8725064bcd424271b8af
harshitha gudikal <harshitha gudikal@LAPTOP-PF4V7P1F>
2021-12-01 19:38:37 +0530

    sample class

--- SampleProject/src/Demo.java
+++ SampleProject/src/Demo.java
@@ -1,7 +1,7 @@
 
 public class Demo {
 	public static void  main(String[] args) {
-		System.out.println("look at the Demo.java");
+		System.out.println("look at Demo.java");
 		
 	}
 
--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -2,7 +2,7 @@
 public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hoo finally in you are in..!");
-		System.out.println("And im done.");
+		System.out.println("And iam trying !");
 		
 	}
 
